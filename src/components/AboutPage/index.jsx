1dd65b04efe00419349ab45288bb044ad4333d32
orms1337 <orms1337@gmail.com>
2021-07-23 15:00:19 +0600

    Add info

--- src/components/AboutPage/index.jsx
+++ src/components/AboutPage/index.jsx
@@ -7,6 +7,27 @@ export default class About extends Component {
     return (
       <div>
         <YandexMap />
+        <div className="content">
+          <div className="content_item">
+            <span className="content_item_title">Телефон</span>
+            <span className="content_item_text">Телефон: 48-78-77</span>
+            <span className="content_item_text">
+              Адрес: Нефтезаводская, д.49б
+            </span>
+          </div>
+          <div className="content_item">
+            <span className="content_item_title">Режим работы</span>
+          </div>
+          <div className="content_item">
+            <span className="content_item_title">Реквизиты</span>
+            <span className="content_item_text">Директор: Сальник С.А.</span>
+            <span className="content_item_text">ОРГН: 120550023882</span>
+            <span className="content_item_text">
+              Юридический адрес: 644040, Омская обл, г.Омск, пр-кт Губкина, д.9,
+              пом. 65Б
+            </span>
+          </div>
+        </div>
       </div>
     );
   }
