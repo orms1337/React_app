4311f3dfdd746aa1d1efb8d4539b35eecf8aced2
orms1337 <orms1337@gmail.com>
2021-07-23 16:19:03 +0600

    Refactor table page

--- src/components/TablePage/index.jsx
+++ src/components/TablePage/index.jsx
@@ -5,75 +5,59 @@ export default class Table extends Component {
   render() {
     return (
       <>
-        <div className="table_cust">
-          <div className="table_row top_discr"> Замена ДВС</div>
-          <div className="table_row">
-            <div className="table_cell_1 bg_cell">
-              Замена двигателя (поперечный/продольный мотор){" "}
-            </div>
-            <div className="table_cell_2 bg_cell">От 8000 р.</div>
+        <div className="table">
+          <span className="table_title">Замена ДВС</span>
+          <div className="table_item">
+            <span className="table_item_text">
+              Замена двигателя (поперечный/продольный мотор)
+            </span>
+            <span className="table_item_text right">От 8000 р.</span>
           </div>
-          <div className="table_row">
-            <div className="table_cell_1 bg_cell">
+          <div className="table_item">
+            <span className="table_item_text">
               Замена двигателя (V-образный мотор)
-            </div>
-            <div className="table_cell_2 bg_cell">От 10 000 р. </div>
+            </span>
+            <span className="table_item_text right">От 10 000 р.</span>
           </div>
-          <div className="table_row top_discr"> Замена КПП</div>
-          <div className="table_row">
-            <div className="table_cell_1 bg_cell">Замена КПП (4wd)</div>
-            <div className="table_cell_2 bg_cell">От 4500 р.</div>
+          <span className="table_title">Замена КПП</span>
+          <div className="table_item">
+            <span className="table_item_text">Замена КПП (4wd)</span>
+            <span className="table_item_text right">От 4500 р.</span>
           </div>
-          <div className="table_row">
-            <div className="table_cell_1 bg_cell">
-              Замена КПП (передний привод){" "}
-            </div>
-            <div className="table_cell_2 bg_cell">От 4000 р. </div>
-          </div>
-        </div>
-        <div className="table_cust">
-          <div className="table_row top_discr"> Замена ДВС</div>
-          <div className="table_row">
-            <div className="table_cell_1 bg_cell">
-              Замена двигателя (передний привод, рядный мотор){" "}
-            </div>
-            <div className="table_cell_2 bg_cell">От 8000 р.</div>
-          </div>
-          <div className="table_row">
-            <div className="table_cell_1 bg_cell">
-              Замена двигателя (V-образный мотор передний привод)
-            </div>
-            <div className="table_cell_2 bg_cell">От 10 000 р. </div>
-          </div>
-          <div className="table_row">
-            <div className="table_cell_1 bg_cell">
-              Замена двигателя (4wd, рядный мотор)
-            </div>
-            <div className="table_cell_2 bg_cell">От 10 000 р.</div>
-          </div>
-          <div className="table_row">
-            <div className="table_cell_1 bg_cell">
-              Замена двигателя (4wd, V-образный мотор)
-            </div>
-            <div className="table_cell_2 bg_cell">От 12 000 р.</div>
-          </div>
-          <div className="table_row top_discr"> Замена КПП</div>
-          <div className="table_row">
-            <div className="table_cell_1 bg_cell">Замена КПП (4wd)</div>
-            <div className="table_cell_2 bg_cell">От 6000 р.</div>
-          </div>
-          <div className="table_row">
-            <div className="table_cell_1 bg_cell">
+          <div className="table_item">
+            <span className="table_item_text">
               Замена КПП (передний привод)
-            </div>
-            <div className="table_cell_2 bg_cell">От 4000 р. </div>
+            </span>
+            <span className="table_item_text right">От 4000 р.</span>
           </div>
         </div>
-        <p className="discription_table">Установка происходит на нашем СТО</p>
-        <p className="discription_table">Срок замены: 1-2 дня;</p>
-        <p className="discription_table">
-          В стоймость замены входит демонтаж старого агрегата;
-        </p>
+
+        <div className="table">
+          <span className="table_title">Замена ДВС</span>
+          <div className="table_item">
+            <span className="table_item_text">
+              Замена двигателя (поперечный/продольный мотор)
+            </span>
+            <span className="table_item_text right">От 8000 р.</span>
+          </div>
+          <div className="table_item">
+            <span className="table_item_text">
+              Замена двигателя (V-образный мотор)
+            </span>
+            <span className="table_item_text right">От 10 000 р.</span>
+          </div>
+          <span className="table_title">Замена КПП</span>
+          <div className="table_item">
+            <span className="table_item_text">Замена КПП (4wd)</span>
+            <span className="table_item_text right">От 4500 р.</span>
+          </div>
+          <div className="table_item">
+            <span className="table_item_text">
+              Замена КПП (передний привод)
+            </span>
+            <span className="table_item_text right">От 4000 р.</span>
+          </div>
+        </div>
       </>
     );
   }
